70bc02edbd8f26ac83d5345778abc251500a1c76
Brant Pippin <brant.pippin@gmail.com>
2021-02-02 09:52:55 -0600

    Comprehensions Over Lists

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,7 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
   end
 
   def shuffle(deck) do
