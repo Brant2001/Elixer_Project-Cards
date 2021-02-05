357f2908cc1db3bdd6aec50260a80391db7089d6
Brant Pippin <brant.pippin@gmail.com>
2021-02-05 11:53:16 -0600

    Case Tests/More Doctests

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,8 +2,19 @@ defmodule Cards do
   @moduledoc """
     Provides methods for creating and handling a deck of cards
   """
-  @moduledoc """
+
+  @doc """
     Creates an array of strings representing a deck of cards
+
+  ## Examples
+
+      iex> Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+       "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+       "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+       "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+       "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+
   """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
@@ -14,6 +25,9 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Takes an array of strings and randomly reorders it.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
@@ -26,7 +40,7 @@ defmodule Cards do
       iex> deck = Cards.create_deck
       iex> Cards.contains?(deck, "Ace of Spades")
       true
-      
+
   """
   def contains?(deck, card) do
     Enum.member?(deck, card)
@@ -48,11 +62,18 @@ defmodule Cards do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Takes an argument (`deck`) and saves it under the desired and given `filename`.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Takes the given `filename` and finds the file associated
+    with it and loads the contents within it.
+  """
   def load(filename) do
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
@@ -60,6 +81,10 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Creates a deck, randomizes it, and returns a list
+    of strings equal to the desired `hand_size`.
+  """
   def create_hand(hand_size) do
     Cards.create_deck()
     |> Cards.shuffle()
